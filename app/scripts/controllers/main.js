73771c1e379031ae39b2ccadd74f8938eb6b3b90
Derek Adair <d@derekadair.com>
2013-09-06 14:26:10 -0500

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('scoreboardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
